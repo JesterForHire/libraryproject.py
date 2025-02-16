f8ce445811288046b9f5af123e9fe8491d3116fd
JesterForHire <comets7432@icloud.com>
2025-02-15 22:15:30 -0800

    Add files via upload

---
+++ libraryproject.py
@@ -0,0 +1,13 @@
+class Book:
+    def __init__(self,name,genre):
+        self.name = name
+        self.genre = genre
+        fantasy[""]
+        action[""]
+        romance[""]
+        horror[""]
+        mystery[""]
+
+
+Aarons_Library_creater = input("Welcome to Aarons Library Creater, Please name your Library")
+Users_Libary = print(str(Aarons_Library_creater) + " WOW what an awesome name!")
